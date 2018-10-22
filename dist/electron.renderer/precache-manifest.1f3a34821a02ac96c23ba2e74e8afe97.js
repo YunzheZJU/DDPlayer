af8d573d4631e1ced1bdabdfa89c097273f8eafe
Yunzhe <iLoveLive@honoka.moe>
2018-10-22 13:51:19 +0800

    Release v0.9.1.

---
+++ dist/electron.renderer/precache-manifest.1f3a34821a02ac96c23ba2e74e8afe97.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2b5d388cfc408954e4b",
+    "url": "/album.3a4beda6.js"
+  },
+  {
+    "revision": "0b043c8e1d1ab7846c66",
+    "url": "/vendors~main.3a4beda6.js"
+  },
+  {
+    "revision": "955bcb1a883e809b2d57",
+    "url": "/collection.15c9111f.css"
+  },
+  {
+    "revision": "5b3d112e778da9c7592f",
+    "url": "/main.f2f00263.css"
+  },
+  {
+    "revision": "65b65814fed6a6a340a4",
+    "url": "/popup.3a4beda6.js"
+  },
+  {
+    "revision": "65b65814fed6a6a340a4",
+    "url": "/popup.2fa99e46.css"
+  },
+  {
+    "revision": "d9f34888f458a98d205c",
+    "url": "/search.3a4beda6.js"
+  },
+  {
+    "revision": "d9f34888f458a98d205c",
+    "url": "/search.7e5d9913.css"
+  },
+  {
+    "revision": "5b3d112e778da9c7592f",
+    "url": "/main.3a4beda6.js"
+  },
+  {
+    "revision": "955bcb1a883e809b2d57",
+    "url": "/collection.3a4beda6.js"
+  },
+  {
+    "revision": "5b442b17898c11970981",
+    "url": "/artist.3a4beda6.js"
+  },
+  {
+    "revision": "5b442b17898c11970981",
+    "url": "/artist.182ac8be.css"
+  },
+  {
+    "revision": "b616854ed031b2e1ce79",
+    "url": "/album~collection~search.3a4beda6.js"
+  },
+  {
+    "revision": "b616854ed031b2e1ce79",
+    "url": "/album~collection~search.33c8e031.css"
+  },
+  {
+    "revision": "d2b5d388cfc408954e4b",
+    "url": "/album.8466bbe9.css"
+  }
+];
---
+++ dist/web/service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/workbox-v3.6.2/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v3.6.2"});
+
+importScripts(
+  "/precache-manifest.bf1af9e8d89af8fff7422d8f1e22fa05.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "DDMusic"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/.+(?:png|gif|jpg|jpeg|svg)$/, workbox.strategies.cacheFirst({ "cacheName":"DDMusic-images", plugins: [new workbox.expiration.Plugin({"maxEntries":50,"maxAgeSeconds":2592000,"purgeOnQuotaError":false}), new workbox.cacheableResponse.Plugin({"statuses":[0,200]})] }), 'GET');
+workbox.routing.registerRoute(/\.(?:js|css)$/, workbox.strategies.staleWhileRevalidate({ "cacheName":"DDMusic-static-resources", plugins: [] }), 'GET');
